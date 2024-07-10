cf15b156188392f532ae43128a3b70012ea2c9d5
AzizSandid <aziz.stage@gmail.com>
2024-07-10 23:37:34 +0100

    feat : add contact section and modify components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Contact from "@/components/Contact";
+import SectionTitle from "@/components/SectionTitle";
 
 export default function Home() {
   return (
-    <main className="flex min-h-[2220px] flex-col items-center justify-between py-20">
-        <Contact/>
+    <main className="flex min-h-[2220px] flex-col items-center justify-between py-24">
+      <div className="flex rounded-3xl h-[512px] w-11/12 bg-primary p-12">
+      <h1 className="text-black font-black sm:font-extrabold text-2xl sm:text-4xl md:text-5xl"
+      >Découvrir Votre Style</h1>
+
+      </div>
+      <Contact/>
     </main>
   );
 }
