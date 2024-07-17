94a8864265dc5849718dab7cf942d9d4b78bd018
labiadhyassin0291 <labiadhyassine9@gmail.com>
2024-07-17 23:25:49 +0100

    hello

--- app/homme/page.tsx
+++ app/homme/page.tsx
@@ -4,8 +4,14 @@
 export default function Page() {
 
 return(
-<div className="flex flex-col overflow-x-hidden min-h-screen items-center">
 
-</div>);
+<div className="flex flex-col overflow-x-hidden min-h-screen items-center py-24 ">
+      <div className="flex rounded-3xl h-[512px] w-11/12 bg-primary px-200 flex justify-center items-center ">
+      <div className="text-black  font-black sm:font-extrabold text-2xl sm:text-4xl md:text-5xl"><h1>Homme</h1>
+      </div> </div><br />
+      <div>trouver les pieces de vos reves</div>
+      
+</div>
 
+);
 }
