4a71da94e0dab12bef06f6ba0984c55bf8510c17
AzizSandid <aziz.stage@gmail.com>
2024-07-21 17:53:33 +0100

    modifier interface d accueil

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,17 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 type CardProps={
   image:string,
   name:string,
-  price:string  
+  price:string,
+  route:string,
 }
-export default function Card ({image,name,price}:CardProps){
-
+export default function Card ({image,name,price,route}:CardProps){
     return(
-        <div className="w-[30%]">
+        <Link href={route} className="w-[30%] shadow rounded-lg p-4 hover:opacity-80">
             <img src={image} alt="Card Image" className="w-[100%] h-[80%] max-h-[400px] max-w-[450px]" />
             <p className="font-medium text-base sm:text-xl md:text-xl py-2 ">{name}</p>
             <p className="font-medium text-sm sm:text-base md:text-xl text-gray">{price}</p>
-        </div>
+        </Link>
     );
 }
